1d23e29f13254741242aab170981f7f1a8c92162
ultrasonicsoft <uvbrainsolutions@outlook.com>
2015-12-14 23:07:18 +0530

    ng2bootstrap

---
+++ test.ts
@@ -0,0 +1,10 @@
+// 1. Component
+// 2. FullComponent with all parameters
+// 3. Service
+// 4. Pipe
+// 5. Directive
+// 6. Service with Http
+// 7. Component, Model, Service
+// 8. EventEmitter
+// 9. Component with CORE_DIRECTIVES, FORM_DIRECTIVES etc
+// 10. Componet with all lifecycle events
