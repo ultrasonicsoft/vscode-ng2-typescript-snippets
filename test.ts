714f7742dd49f9cd687288e305ec1b2ae7d38c4f
ultrasonicsoft <uvbrainsolutions@outlook.com>
2015-12-16 22:05:01 +0530

    component empty

--- test.ts
+++ test.ts
@@ -8,24 +8,9 @@
 // 8. EventEmitter
 // 9. Component with CORE_DIRECTIVES, FORM_DIRECTIVES etc
 // 10. Componet with all lifecycle events
-
-
-import {Pipe} from 'angular2/core';
-@Pipe({
-	name: 'fetch',
-	pure: false
-})
-export class asyncPipePipe {
-	private fetchedValue: any;
-	private fetchPromise: Promise<any>;
-	@Output() eventName = new EventEmitter();
 	
-	transform(value: string, args: string[]): any {
-		if (!this.fetchPromise) {
-			this.fetchPromise = window.fetch(value)
-				.then((result: any) => result.json())
-				.then((json: any) => this.fetchedValue = json);
-		}
-		return this.fetchedValue;
+	import {Component } from 'angular2/core';
+	@Component({selector: 'componentName', template: '<h2>This is componentName component.</h2><h3>Name: {{ name }}</h3>}'})
+	class componentName {
+	  name: string = 'Angular2 User';
 	}
-}
